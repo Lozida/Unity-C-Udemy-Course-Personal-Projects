c0f58eb10f07004d32e95dd5fde6b18fc10a606c
Lozida <80642162+Lozida@users.noreply.github.com>
2021-03-14 20:49:18 +0100

    Project 6

---
+++ 5_Zombie_Runner/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    [SerializeField] float chaseDistance = 5f;
+    [SerializeField] float chaseRange = 10f;
+    
+    NavMeshAgent navMeshAgent; // Getting the NavMeshAgent component from UnityEngine.AI.
+    float distanceToTarget = Mathf.Infinity; // Infinite number.
+    bool isProvoked = false;
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        distanceToTarget = Vector3.Distance(target.position, transform.position); // .Distance = returns the value of A to B.
+        if (isProvoked)
+        {
+            EngageTarget();
+        }
+        else if (distanceToTarget <= chaseDistance)
+        {
+            isProvoked = true;
+        }
+    }
+
+    private void EngageTarget()
+    {
+        if (distanceToTarget >= navMeshAgent.stoppingDistance)
+        {
+            ChaseTarget();
+        }
+        if (distanceToTarget <= navMeshAgent.stoppingDistance)
+        {
+            AttackTarget();
+        }
+    }
+
+    private void ChaseTarget()
+    {
+        navMeshAgent.SetDestination(target.position);
+    }
+
+    private void AttackTarget()
+    {
+        Debug.Log(name + "has seeked and is destroying" + target.name);
+    }
+
+    void OnDrawGizmosSelected() // Creating a sphere that shows the distance between enemy and player.
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, chaseRange);
+    }
+}
+
---
+++ 5_Zombie_Runner/Assets/Scripts/Weapon.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    [SerializeField] Camera FPCamera;
+    [SerializeField] float range = 100f;
+    [SerializeField] float damage = 25f;
+    [SerializeField] ParticleSystem muzzleFlash;
+    [SerializeField] GameObject hitEffect;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1")) // Getting the fire button.
+        {
+            Shoot(); // Shoot method.
+        }
+    }
+
+    private void Shoot()
+    {
+        PlayMuzzleFlash();
+        ProcessRaycast();
+
+    }
+
+    private void ProcessRaycast()
+    {
+        RaycastHit hit; // Creating a RaycastHit var.
+        if (Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range)) // If is for protection againts null reference error.
+        {
+            CreateHitImpact(hit);
+            Debug.Log("You hit: " + hit.transform.name);
+            EnemyHealth target = hit.transform.GetComponent<EnemyHealth>();
+            if (target == null) return;
+            target.TakeDamage(damage);
+        }
+        else
+        {
+            return;
+        }
+    }
+
+    private void CreateHitImpact(RaycastHit hit)
+    {
+        GameObject impact = Instantiate(hitEffect, hit.point, Quaternion.LookRotation(hit.normal));
+        Destroy(impact, .1f);
+    }
+
+    private void PlayMuzzleFlash() 
+    {
+        muzzleFlash.Play();
+    }
+}
